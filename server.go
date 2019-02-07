7a652aa994318462acf9a842033207c76856413a
Kuan-Ying Fang <kuanyingfang@gmail.com>
2019-02-07 15:13:51 -0800

    implementing gorm persistence

--- server.go
+++ server.go
@@ -4,16 +4,25 @@ package main
 
 import (
 	"net/http"
-    // "fmt"
+    "fmt"
 	"github.com/labstack/echo"
     "encoding/json"
     "io/ioutil"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func main() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
 	e := echo.New()
 
     type Song struct{
+		gorm.Model
         Title string `json:"title"`
     }
 
@@ -21,8 +30,14 @@ func main() {
         Songs []Song
     }
 
+	db.AutoMigrate(&Song{})
+
     // Display the list of song titles
 	e.GET("/", func(c echo.Context) error {
+        context_ip := c.RealIP()
+        fmt.Println(context_ip)
+        context_req := c.Request()
+        fmt.Println(context_req)
         pattern := c.QueryParam("pattern")
         if len(pattern) == 0{
             pattern = "Marley"
@@ -51,6 +66,10 @@ func main() {
             panic(err1)
         }
 
+		for _, songname := range responseSongs.Songs {
+			db.Create(&songname)
+		}
+
         // fmt.Println(responseSongs)
         // can't print this. its a struct, containing an array.
 
@@ -62,5 +81,13 @@ func main() {
         return c.JSON(http.StatusOK, responseSongs.Songs)
 		// return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.GET("/get-stored", func(c echo.Context) error {
+		// var songsList SongsList
+		songsList := SongsList{}
+		db.Find(&songsList.Songs)
+		return c.JSON(http.StatusOK, songsList.Songs)
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
